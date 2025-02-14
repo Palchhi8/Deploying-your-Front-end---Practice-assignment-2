f56bbd3249910ad112932127d03974a45d7314d2
Palchhi8 <jainpalchhi2k6@gmail.com>
2025-02-14 09:53:37 +0530

    deploying-front-end

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import styles from "./styles/Home.module.css";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Palchhi Jain</h1>
+      <div className={styles.card}>
+        <p className={styles.contact}>📧 Email: jainpalchhi2k6@gmail.com</p>
+        <p className={styles.contact}>📞 Phone: +919351885259</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/Home.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Palchhi Jain</h1>
+        <p>Email: palchhijain2k6@gmail.com</p>
+        <p>Phone: +919351885259</p>
+      </div>
+    );
+  }
+  
+  export default App;
+  
